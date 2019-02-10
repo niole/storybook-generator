317eef5b8df916f66ac9884fb762bb9641b0fc61
Niole Nelson <niolenelson@Nioles-MBP.lan>
2019-02-10 15:42:44 -0800

    Add numbers strings to object union test

---
+++ test/objectUnionTypes.test.ts
@@ -0,0 +1,44 @@
+import * as test from 'tape';
+import { createSourceFile, ScriptTarget, SourceFile } from 'typescript/lib/typescript';
+
+import { findTypeDeclarations } from '../parser';
+
+test('unnamed object with numbers union test', t => {
+    t.plan(4);
+
+    const union = 'type W = { x: 1, y: 1, } | { x: 2, y: 2 };';
+
+    const sourceFile: SourceFile = createSourceFile('x.ts', union, ScriptTarget.ES5);
+    const decs = findTypeDeclarations(sourceFile);
+
+    t.equal(decs.length, 1, 'total decalarations found should be one');
+
+    const dec = decs[0];
+
+    const firstObject = { x: 1, y: 1, };
+    const secondObject = { x: 2, y: 2 };
+    const fomattedFirstObject = JSON.stringify(firstObject);
+    t.deepEqual(dec.get(), firstObject, `first get() should return ${fomattedFirstObject}`);
+    t.deepEqual(dec.get(), secondObject, `second get() should return ${JSON.stringify(secondObject)}`);
+    t.deepEqual(dec.get(), firstObject, `third get() should return ${fomattedFirstObject}`);
+});
+
+test('unnamed object with numbers and strings union test', t => {
+    t.plan(4);
+
+    const union = 'type W = { x: 1, y: "1", } | { x: 2, y: "2" };';
+
+    const sourceFile: SourceFile = createSourceFile('x.ts', union, ScriptTarget.ES5);
+    const decs = findTypeDeclarations(sourceFile);
+
+    t.equal(decs.length, 1, 'total decalarations found should be one');
+
+    const dec = decs[0];
+
+    const firstObject = { x: 1, y: "1", };
+    const secondObject = { x: 2, y: "2" };
+    const fomattedFirstObject = JSON.stringify(firstObject);
+    t.deepEqual(dec.get(), firstObject, `first get() should return ${fomattedFirstObject}`);
+    t.deepEqual(dec.get(), secondObject, `second get() should return ${JSON.stringify(secondObject)}`);
+    t.deepEqual(dec.get(), firstObject, `third get() should return ${fomattedFirstObject}`);
+});
