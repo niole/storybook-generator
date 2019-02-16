0bdd835e516d4d4e193e13fe47cb6db9c80497ab
Niole Nelson <niolenelson@Nioles-MBP.lan>
2019-02-16 11:48:40 -0800

    Add test for multi depth type declarations

--- test/objectTypeDeclarations.test.ts
+++ test/objectTypeDeclarations.test.ts
@@ -29,3 +29,35 @@ test('should be able to parse single depth type declaration objects', t => {
 
     t.deepEquals(generators[0].get(), expected, `should equal ${JSON.stringify(expected)}`);
 });
+
+ test('should be able to parse multi depth type declaration objects', t => {
+     t.plan(2);
+     const typeDeclaration = `
+         type DeclarationName = {
+             xyz: {
+                 TTT: string;
+                 catsz: object;
+             };
+             Z: boolean; abc: number;
+         };
+     `;
+     const out = createSourceFile('x.ts', typeDeclaration, ScriptTarget.ES5);
+     const generators = findTypeDeclarations(out);
+ 
+     const xyzGenerator = {
+        TTT: getStringGenerator('DeclarationNamexyzTTT')(),
+        catsz: {},
+     };
+     const ZGenerator = getBooleanGenerator();
+     const abcGenerator = getNumberGenerator('DeclarationNameabc');
+ 
+     const expected = {
+         xyz: xyzGenerator,
+         Z: ZGenerator(),
+         abc: abcGenerator(),
+     };
+ 
+     t.equals(generators.length, 1, 'should find one generator');
+ 
+     t.deepEquals(generators[0].get(), expected, `should equal ${JSON.stringify(expected)}`);
+ });
